0a027c33c3851e44afd8242384ab9488c6b07f7c
System Administrator <root@Pavels-MacBook-Pro.local>
2022-03-06 23:22:32 +0200

    Display a list of Room items in the Preview

--- ConferenceApp-iOS/ContentView.swift
+++ ConferenceApp-iOS/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    var rooms: [Room] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(rooms) { room in
+                HStack {
+                    Image(systemName: "photo") // room.thumnail
+                        .cornerRadius(8)
+                    VStack(alignment: .leading) {
+                        Text(room.name)
+                        Text("\(room.capacity) people")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Rooms"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rooms: testData)
     }
 }
