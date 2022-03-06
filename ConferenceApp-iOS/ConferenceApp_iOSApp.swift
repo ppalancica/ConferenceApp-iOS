2c244bf75bb1bdf8f91a016fc7669e2f42f6dc29
System Administrator <root@Pavels-MacBook-Pro.local>
2022-03-06 22:13:51 +0200

    First commit. Generated Xcode project with SwiftUI configurations

---
+++ ConferenceApp-iOS/ConferenceApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConferenceApp_iOSApp.swift
+//  ConferenceApp-iOS
+//
+//  Created by Pavel Palancica on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ConferenceApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
