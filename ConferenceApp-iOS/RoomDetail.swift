3479787246b05c37fc03675eaf9522bd2f98ea43
System Administrator <root@Pavels-MacBook-Pro.local>
2022-03-07 02:30:25 +0200

    Added zooming functionality

--- ConferenceApp-iOS/RoomDetail.swift
+++ ConferenceApp-iOS/RoomDetail.swift
@@ -9,17 +9,37 @@ import SwiftUI
 
 struct RoomDetail: View {
     let room: Room
+    @State private var zoomed = false
+    
     var body: some View {
-        Image(room.imageName) // Image(systemName: "photo")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .navigationBarTitle(Text(room.name), displayMode: .inline)
+        ZStack(alignment: .topLeading) {
+            Image(room.imageName) // Image(systemName: "photo")
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .navigationBarTitle(Text(room.name), displayMode: .inline)
+                .onTapGesture {
+                    withAnimation(.linear(duration: 2)) { self.zoomed.toggle()
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            
+            if room.hasVideo && !zoomed {
+                Image(systemName: "video.fill")
+                    .font(.title)
+                    .padding(.all)
+                    .transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
 struct RoomDetail_Previews: PreviewProvider {
     static var previews: some View {
         // RoomDetail(room: testData[0])
-        NavigationView { RoomDetail(room: testData[0]) }
+        // NavigationView { RoomDetail(room: testData[0]) }
+        Group {
+            NavigationView { RoomDetail(room: testData[0]) }
+            NavigationView { RoomDetail(room: testData[1]) }
+        }
     }
 }
